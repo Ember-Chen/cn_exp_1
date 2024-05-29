efead7de0159a9fcbdb5fb5142f8029215dbf597
Vu1c4n <1633080461@qq.com>
2024-05-29 08:44:31 +0800

    refactor: 修改client

--- Client.java
+++ Client.java
@@ -1,25 +1,31 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Client {
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8080;
+    private static String path;
     public static void main(String[] args) {
-        String host = "127.0.0.1";
-        int port = 8080;
-        String path = "/a.html";
-
+        System.out.print("plz enter url: " + HOST + ":" + PORT);
+        Scanner sc = new Scanner(System.in);
+        path = sc.nextLine();
+        Socket socket;
+        PrintWriter out;
+        BufferedReader in;
         try {
             // 创建Socket连接到服务器
-            Socket socket = new Socket(host, port);
+            socket = new Socket(HOST, PORT); // 连接到HOST:PORT
 
             // 创建输出流，发送HTTP请求
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out = new PrintWriter(socket.getOutputStream(), true);
             out.println("GET " + path + " HTTP/1.1");
-            out.println("Host: " + host);
+            out.println("HOST: " + HOST);
             out.println("Connection: Close");
             out.println(); // 空行表示请求头结束
 
             // 创建输入流，读取服务器响应
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
                 System.out.println(inputLine);
@@ -29,11 +35,12 @@ public class Client {
             in.close();
             out.close();
             socket.close();
+            sc.close();
         } catch (UnknownHostException e) {
-            System.err.println("Don't know about host: " + host);
+            System.err.println("Don't know about HOST: " + HOST);
             e.printStackTrace();
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to: " + host);
+            System.err.println("Couldn't get I/O for the connection to: " + HOST);
             e.printStackTrace();
         }
     }
